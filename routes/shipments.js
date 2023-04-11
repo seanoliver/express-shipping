e0589e9a06f014a61cadc1c34e3e88941ed63378
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-04-11 16:31:36 -0700

    added shipments schema

--- routes/shipments.js
+++ routes/shipments.js
@@ -1,5 +1,8 @@
 "use strict";
 
+const jsonschema = require("jsonschema");
+const shipitSchema = require("../schemas/shipitSchema.json");
+
 const express = require("express");
 const { BadRequestError } = require("../expressError");
 const router = new express.Router();
@@ -18,8 +21,16 @@ router.post("/", async function (req, res, next) {
   if (req.body === undefined) {
     throw new BadRequestError();
   }
+  const result = jsonschema.validate(req.body, shipitSchema, {required: true});
+
+  if (!result.valid) {
+    const errors = result.errors.map(error => error.stack);
+    throw new BadRequestError(errors);
+
+  }
   const { productId, name, addr, zip } = req.body;
   const shipId = await shipProduct({ productId, name, addr, zip });
+  
   return res.json({ shipped: shipId });
 });
 
