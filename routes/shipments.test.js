2776a2195c1d18f0c23fe454bff57ac3378950e2
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-04-11 16:55:07 -0700

    added feedback from code review

--- routes/shipments.test.js
+++ routes/shipments.test.js
@@ -16,10 +16,37 @@ describe("POST /", function () {
     expect(resp.body).toEqual({ shipped: expect.any(Number) });
   });
 
+  test("returns error JSON if inputs are invalid", async function () {
+    const testShipment = {
+      productId: 4,
+      name: 1,
+      zip: 12345,
+      size: "large"
+    }
+    // TODO: Break into two separate tests (right items but wrong values; wrong
+    // items)
+    // TODO: Separate status code expect statement
+    const resp = await request(app).post("/shipments").send(testShipment);
+    expect(resp.body).toEqual({
+      "error": {
+        "message": [
+          "instance.productId must be greater than or equal to 1000",
+          "instance.name is not of a type(s) string",
+          "instance.zip is not of a type(s) string",
+          "instance is not allowed to have the additional property \"size\"",
+          "instance requires property \"addr\""
+        ],
+        "status": 400
+      }
+    });
+  })
+
   test("throws error if empty request body", async function () {
     const resp = await request(app)
       .post("/shipments")
       .send();
     expect(resp.statusCode).toEqual(400);
   });
+
+
 });
